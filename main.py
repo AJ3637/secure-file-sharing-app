bd31daa548bc6948bf6c6bf3e29f57b52ffb11a3
AJ3637 <cyberwork360@gmail.com>
2025-08-03 16:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -315,9 +315,14 @@ def forgot_password():
 
 
 # ================= RUN APP =================
+#if __name__ == '__main__':
+  #  port = int(os.environ.get("PORT", 5000))
+   # app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+
+
 
 
 
